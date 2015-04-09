d9262db77a57e7b74ab4a9eb7ac5076ec128f85b
wolf-mtwo <wolf.mtwo@outlook.com>
2015-04-09 10:17:14 +0200

    Adding Db example

--- public/controllers/sysadmin.js
+++ public/controllers/sysadmin.js
@@ -1,10 +1,24 @@
 'use strict';
 
 angular.module('seedApp')
-.controller('SysadminController', ['$scope', '$state',
-  function($scope, $state) {
+.controller('SysadminController', ['$scope', '$state', 'Person',
+  function($scope, $state, Person) {
   	$scope.cerrarSession = function() {
   		$state.go('home')
   	}
+
+
+  	$scope.persons = [];
+    Person.get({}, function(response) {
+      $scope.persons = response;
+    });
+
+    $scope.item = {};
+
+    $scope.savePerson = function(item) {
+      Person.save(item, function(response) {
+        $scope.persons.push(response);
+      });
+    }
   }
 ]);
