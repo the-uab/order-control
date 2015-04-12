7cfb64994e1b4d3b539f49983c68bf53b68bd04c
Franko <franko.2365@gmail.com>
2015-04-12 01:27:50 -0400

    Resolving error login

--- public/controllers/sessions.js
+++ public/controllers/sessions.js
@@ -5,16 +5,14 @@ angular.module('seedApp')
   function($scope, $state) {
   	$scope.login = function(item) {
   		console.log(item);
-<<<<<<< HEAD
-  		$state.go('sysadmin');
-=======
+
   		if (item.email == "sysadmin") {
 
-	  		$state.go('sysadmin');	
+	  		$state.go('treasure');	
   		} else {
   			$state.go('treasure');
   		}
->>>>>>> 33b839b0c5684a5a2be0eaf84a37ae86004c78a7
+
   	}	
   }
 ]);
