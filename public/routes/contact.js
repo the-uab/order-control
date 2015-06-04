be7db8a0eb01e94be664f76ee6a2ad93c3360e65
wolf-mtwo <wolf.mtwo@outlook.com>
2015-06-04 05:12:04 +0200

    Refacting the proejct

--- public/routes/contact.js
+++ public/routes/contact.js
@@ -12,88 +12,88 @@ angular.module('seedApp')
       controller: 'ContactController'
     });
 
-    $stateProvider.state('home.contact.dev', {
-      url: '/dev',
-      templateUrl: path + '/dev.html',
-      controller: 'ContactController'
-    });
+    // $stateProvider.state('home.contact.dev', {
+    //   url: '/dev',
+    //   templateUrl: path + '/dev.html',
+    //   controller: 'ContactController'
+    // });
 
-    $stateProvider.state('home.contact.aguti666', {
-      url: '/aguti666',
-      templateUrl: path + '/aguti666.html',
-      controller: 'HomeController'
-    });
+    // $stateProvider.state('home.contact.aguti666', {
+    //   url: '/aguti666',
+    //   templateUrl: path + '/aguti666.html',
+    //   controller: 'HomeController'
+    // });
 
-    $stateProvider.state('home.contact.pedidos1', {
-      url: '/pedidos1',
-      templateUrl: path + '/pedidos1.html',
-      controller: 'HomeController'
-    });
+    // $stateProvider.state('home.contact.pedidos1', {
+    //   url: '/pedidos1',
+    //   templateUrl: path + '/pedidos1.html',
+    //   controller: 'HomeController'
+    // });
 
-    $stateProvider.state('home.contact.pedidos2', {
-      url: '/pedidos2',
-      templateUrl: path + '/pedidos2.html',
-      controller: 'HomeController'
-    });
+    // $stateProvider.state('home.contact.pedidos2', {
+    //   url: '/pedidos2',
+    //   templateUrl: path + '/pedidos2.html',
+    //   controller: 'HomeController'
+    // });
 
-    $stateProvider.state('home.contact.Vivian', {
-      url: '/Vivian',
-      templateUrl: path + '/Vivian.html',
-      controller: 'HomeController'
-    });
+    // $stateProvider.state('home.contact.Vivian', {
+    //   url: '/Vivian',
+    //   templateUrl: path + '/Vivian.html',
+    //   controller: 'HomeController'
+    // });
 
-    $stateProvider.state('home.contact.detallePedido', {
-      url: '/detallePedido',
-      templateUrl: path + '/detallePedido.html',
-      controller: 'HomeController'
-    });
+    // $stateProvider.state('home.contact.detallePedido', {
+    //   url: '/detallePedido',
+    //   templateUrl: path + '/detallePedido.html',
+    //   controller: 'HomeController'
+    // });
 
-    $stateProvider.state('home.contact.editarPedido', {
-      url: '/editarPedido',
-      templateUrl: path + '/editarPedido.html',
-      controller: 'HomeController'
-    });
+    // $stateProvider.state('home.contact.editarPedido', {
+    //   url: '/editarPedido',
+    //   templateUrl: path + '/editarPedido.html',
+    //   controller: 'HomeController'
+    // });
 
-    $stateProvider.state('home.contact.mariouab', {
-      url: '/mariouab',
-      templateUrl: path + '/mariouab.html',
-      controller: 'HomeController'
-    });
+    // $stateProvider.state('home.contact.mariouab', {
+    //   url: '/mariouab',
+    //   templateUrl: path + '/mariouab.html',
+    //   controller: 'HomeController'
+    // });
 
-    $stateProvider.state('home.contact.elias', {
-      url: '/elias',
-      templateUrl: path + '/elias.html',
-      controller: 'HomeController'
-    });
+    // $stateProvider.state('home.contact.elias', {
+    //   url: '/elias',
+    //   templateUrl: path + '/elias.html',
+    //   controller: 'HomeController'
+    // });
 
-    $stateProvider.state('home.contact.finanzas2', {
-      url: '/finanzas2',
-      templateUrl: path + '/finanzas2.html',
-      controller: 'HomeController'
-    });
+    // $stateProvider.state('home.contact.finanzas2', {
+    //   url: '/finanzas2',
+    //   templateUrl: path + '/finanzas2.html',
+    //   controller: 'HomeController'
+    // });
 
-    $stateProvider.state('home.contact.ronald', {
-      url: '/ronald',
-      templateUrl: path + '/ronald.html',
-      controller: 'HomeController'
-    });
+    // $stateProvider.state('home.contact.ronald', {
+    //   url: '/ronald',
+    //   templateUrl: path + '/ronald.html',
+    //   controller: 'HomeController'
+    // });
 
-    $stateProvider.state('home.contact.person', {
-      url: '/person',
-      templateUrl: path + '/person.html',
-      controller: 'PersonController'
-    });
+    // $stateProvider.state('home.contact.person', {
+    //   url: '/person',
+    //   templateUrl: path + '/person.html',
+    //   controller: 'PersonController'
+    // });
 
-    $stateProvider.state('home.contact.gere', {
-      url: '/gere',
-      templateUrl: path + '/gere.html',
-      controller: 'HomeController'
-    });
+    // $stateProvider.state('home.contact.gere', {
+    //   url: '/gere',
+    //   templateUrl: path + '/gere.html',
+    //   controller: 'HomeController'
+    // });
 
-    $stateProvider.state('home.contact.gereDos', {
-      url: '/gereDos',
-      templateUrl: path + '/gereDos.html',
-      controller: 'HomeController'
-    });
+    // $stateProvider.state('home.contact.gereDos', {
+    //   url: '/gereDos',
+    //   templateUrl: path + '/gereDos.html',
+    //   controller: 'HomeController'
+    // });
   }
 ]);
--- public/routes/home.js
+++ public/routes/home.js
@@ -4,36 +4,38 @@ angular.module('seedApp')
   function($urlRouterProvider, $stateProvider) {
     var path = './public/views';
 
-    $stateProvider.state('home', {
+    $stateProvider.state('main', {
       url: '/',
-      templateUrl: path + '/home.html',
+      templateUrl: path + '/index.html',
       controller: 'MainController'
     });
 
-    $stateProvider.state('home.session', {
+
+
+    $stateProvider.state('main.session', {
       url: 'session',
       templateUrl: path + '/session.html',
       controller: 'SessionsController'
     });
-// tesorero
-    $stateProvider.state('home.treasurer', {
-      url: 'treasurer',
+
+    $stateProvider.state('treasurer', {
+      url: '/treasurer',
       templateUrl: path + '/treasurer.html',
-      controller: 'MainController'
+      controller: 'TreasureController'
     });
+
     $stateProvider.state('sysadmin', {
       url: '/sysadmin',
       templateUrl: path + '/sysadmin.html',
       controller: 'SysadminController'
     });
 
-    //     -/sysadmin/newuser
     $stateProvider.state('sysadmin.newuser', {
       url: '/newuser',
       templateUrl: path + '/newuser.html',
       controller: 'UsersController'
     });
-    //Tesorero -->Treasure
+
     $stateProvider.state('treasure', {
       url: '/',
       templateUrl: path + '/treasure.html',
